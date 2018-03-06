1be30bdf6fccd9ad291f3f035e6e421b6ada96c3
R.S. Bhalla <rsbhalla@users.noreply.github.com>
2018-03-06 19:24:21 +0530

    Loops converted to functions

--- TBRG/tbrg_nlg.R
+++ TBRG/tbrg_nlg.R
@@ -1,6 +1,9 @@
  ## This script is meant to hold the variables for Nilgiris and call sub routines.
 library(timeSeries)
 library(ggplot2)
+library(parallel)
+
+source("myfuncts.R", echo=FALSE)
 
 ## set the financial centre
 setRmetricsOptions(myFinCenter = "Asia/Calcutta")
@@ -8,36 +11,34 @@ Sys.setenv(TZ='Asia/Kolkata')
 ## setRmetricsOptions(tz = "Asia/Kolkata")
 
 site <- "nilgiris."
-tbrgdatadir<-"/home/udumbu/rsb/OngoingProjects/CWC/Data/Nilgiris/tbrg/raw"
-tbrg_nulldatadir<-"/home/udumbu/rsb/OngoingProjects/CWC/Data/Nilgiris/tbrg/null"
+tbrgdatadir<-"~/Res/CWC/Data/Nilgiris/tbrg/raw"
+tbrg_nulldatadir<-"~/Res/CWC/Data/Nilgiris/tbrg/null"
 ## wkdir<-"/home/udumbu/rsb/OngoingProjects/CWC/rdata/"
-wkdir <-"/home/udumbu/rsb/GitHub/CWC/TBRG/"
+wkdir <-"~/Res/CWC/Anl/TBRG/"
 setwd(wkdir)
 ## Create tables to hold the tbrg and wlr datasets
-num_tbrg <- c(101:135, "110a")
+num_tbrg <- c(101:135, "105a","110a", "125a", "134a") # testing
+## num_tbrg <- c(101:105)
 ## Ensure the diretories for the data are created
 ## in the console "mkdir tbrg_{101..130}" will create directories tbrg_101 to tbrg_130
-calibfile <- "/home/udumbu/rsb/OngoingProjects/CWC/Data/Nilgiris/tbrg/calib/nilgiri_tbrg_calibration_fnl.csv"
-figdir <- "/home/udumbu/rsb/OngoingProjects/CWC/Data/Nilgiris/tbrg/fig/"
-csvdir <- "/home/udumbu/rsb/OngoingProjects/CWC/Data/Nilgiris/tbrg/csv/"
+calibfile <- "~/Res/CWC/Data/Nilgiris/tbrg/calib/nilgiri_tbrg_calibration_fnl.csv"
+figdir <- "~/Res/CWC/Data/Nilgiris/tbrg/fig/"
+csvdir <- "~/Res/CWC/Data/Nilgiris/tbrg/csv/"
 
-for (i in 1: length(num_tbrg)){ ## to be replaced with foreach command
-    ## resultlist <- foreach(i=1:length(num_tbrg)) %dopar% { ## this needs fixing
-    ## List the names of the files
-    tbrgtab<-paste("tbrg_", num_tbrg[i], sep="") # Directory/table per tbrg holding csv files are stored.
-    tbrgtab_raw<-paste("tbrg_", num_tbrg[i], "_raw", sep="")
-    tbrgtab_pseudo<-paste("tbrg_", num_tbrg[i], "_pdeudo", sep="")
-    tbrgdir<-paste(tbrgdatadir, tbrgtab, sep="/") # Directory holding all tbrg sub folders
-    tbrgdir_null<-paste(tbrg_nulldatadir, tbrgtab, sep="/") # Directory holding all tbrg_null sub folders
-    tbrgtab_null_all<-paste(tbrgtab, "_null_all",sep="") # Directory holding all tbrg_null sub folders
-    filelist <- list.files(tbrgdir, pattern="\\.csv$|\\.dat$", ignore.case=TRUE, full.names=FALSE)
-    filelist.full <- list.files(tbrgdir, pattern="\\.csv$|\\.dat$" , ignore.case=TRUE, full.names=TRUE)
-    filelist_null<- list.files(tbrgdir_null, pattern="\\.csv$|\\.dat$" , ignore.case=TRUE, full.names=FALSE)
-    filelist_null.full <- list.files(tbrgdir_null, pattern="\\.csv$|\\.dat$", ignore.case=TRUE, full.names=TRUE)
-    csv.out <- paste(csvdir, tbrgtab, "_onemin.csv", sep="")
-## source(paste(wkdir,"myfuncts.R", sep=""), echo=TRUE)
-    source(paste(wkdir,"tbrg_import.R", sep=""), echo=TRUE)
-    source(paste(wkdir,"tbrg_fillnull.R", sep=""), echo=TRUE)
-    source(paste(wkdir,"tbrg_aggreg.R", sep=""), echo=TRUE)
-    
-}
+## Set up the cores for multi-core processing to speed up stuff
+## remember there is quite a bit of writing to disk so this won't
+## necessarily speed things up too much.
+##---------WARNING-------------###
+## ONLY WORKS ON LINUX BOXES.
+## visit this page to see how to set up for windows
+## <https://www.r-bloggers.com/a-no-bs-guide-to-the-basics-of-parallelization-in-r/>
+
+## Fix the year on data downloaded 2016 and after - run only once
+## note original data needs to be stored in folder named as below
+x <- list.files("~/Res/CWC/Data/Nilgiris/tbrg/original.raw/", full.names=TRUE, recursive=TRUE)
+lapply(x, fix.tbrg.yr)
+
+use.cores <- detectCores() - 1 # use all but one cores
+mclapply(X=num_tbrg, FUN=control.funct, mc.cores=use.cores)
+
+print("Finished processing TBRG data for Nilgiris")
